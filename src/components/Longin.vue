9356eb504476f712eff3bbc8728c6ac49fb51896
sunchao1105 <sunchao1105@qq.com>
2019-07-24 13:40:36 +0800

    增加登录提示

--- src/components/Longin.vue
+++ src/components/Longin.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="body">
     <div class="login">
+      <h4>用户登录</h4>
       <Form>
         <FormItem>
           <Input prefix="md-contact" v-model="operForm.username" placeholder="请输入账号" style="width: 250px;height:45px" />
@@ -37,11 +38,19 @@ export default {
   .login{
     margin: auto;
     width: 400px;
-    padding-top: 50px;
+    padding-top: 30px;
     padding-bottom: 30px;
     background-color: #fff;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    h4{
+      margin-bottom: 30px;
+      height: 50px;
+      line-height: 50px;
+      font-size: 18px;
+      color: #4A94FA;
+    }
   }
 }
 </style>
